5936679dd30bdb52f8ef5758cbdb3fce246861f4
Griffiths, Ethan (Data61, Pullenvale) <ethan.griffiths@data61.csiro.au>
2024-05-10 07:54:24 +1000

    Remove old splits from eval list

--- eval/utils.py
+++ eval/utils.py
@@ -8,16 +8,20 @@ def get_query_database_splits(params):
         eval_database_files = ['DCC_database.pickle', 'Sejong_database.pickle']
         eval_query_files = ['DCC_queries.pickle', 'Sejong_queries.pickle']
     elif 'AboveUnder' in params.dataset_name:
-        eval_database_files = ['above-under_Beetaloo_evaluation_database.pickle',
-                               'above-under_Karawatha_evaluation_database.pickle',
-                               'above-under_QCAT_evaluation_database.pickle', 
-                               'above-under_Robson_evaluation_database.pickle', 
-                               'above-under_Samford_evaluation_database.pickle']
-        eval_query_files = ['above-under_Beetaloo_evaluation_query.pickle',
-                            'above-under_Karawatha_evaluation_query.pickle',
-                            'above-under_QCAT_evaluation_query.pickle', 
-                            'above-under_Robson_evaluation_query.pickle', 
-                            'above-under_Samford_evaluation_query.pickle']
+        eval_database_files = [
+            # 'above-under_Beetaloo_evaluation_database.pickle',
+            'above-under_Karawatha_evaluation_database.pickle',
+            'above-under_QCAT_evaluation_database.pickle', 
+            # 'above-under_Robson_evaluation_database.pickle', 
+            'above-under_Samford_evaluation_database.pickle'
+        ]
+        eval_query_files = [
+            # 'above-under_Beetaloo_evaluation_query.pickle',
+            'above-under_Karawatha_evaluation_query.pickle',
+            'above-under_QCAT_evaluation_query.pickle', 
+            # 'above-under_Robson_evaluation_query.pickle', 
+            'above-under_Samford_evaluation_query.pickle'
+        ]
     elif params.dataset_name == 'Campus3D':
         eval_database_files = ['umd_evaluation_database.pickle']
         eval_query_files = ['umd_evaluation_query_PNVLAD_format.pickle']
