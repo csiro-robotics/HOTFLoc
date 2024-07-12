Implement normalisation by fixed scale factor, remove points outside of [-1, 1] after data aug

dataset/augmentation.py
@@ -1,5 +1,6 @@
 import math
 import random
+from typing import Optional
 
 import numpy as np
 import torch
@@ -181,16 +182,41 @@ class RemoveRandomBlock:
 
 class Normalize:
     """
-    Normalize cloud to [-scale, scale] range.
-    """    
-    def __init__(self, scale: float=1.0):
-        assert scale > 0, 'Scale must be positive'
-        self.scale = scale
+    Normalize cloud within `[-norm_range, norm_range]`. Defaults to `[-1, 1]`.
 
-    def __call__(self, coords):
+    Alternatively, provide `scale_factor` to normalize the cloud with a fixed
+    scaling factor. E.g. `cloud_normalized = (cloud - centroid) / scale_factor`.
+    """    
+    def __init__(self, norm_range: Optional[float] = None,
+                 scale_factor: Optional[float] = None):
+        assert not all([arg is not None for arg in [norm_range, scale_factor]]),\
+            "Must specify one of norm_range or scale_factor, not both"
+        self.norm_range = 1.0
+        self.scale_factor = None
+        if norm_range is not None:
+            assert norm_range > 0, "Range must be positive"
+            self.norm_range = norm_range
+        elif scale_factor is not None:
+            assert scale_factor > 0, "Scale factor must be positive"
+            self.norm_range = None
+            self.scale_factor = scale_factor
+
+    def __call__(self, coords: torch.Tensor):
         bbmin = coords.min(dim=0).values
         bbmax = coords.max(dim=0).values
         center = (bbmin + bbmax) * 0.5
-        box_size = (bbmax - bbmin).max() + 1.0e-6
-        coords = (coords - center) * (2.0 * self.scale / box_size)
+        if self.scale_factor is not None:
+            coords = (coords - center) / self.scale_factor
+        else:
+            box_size = (bbmax - bbmin).max() + 1.0e-6
+            coords = (coords - center) * (2.0 * self.norm_range / box_size)
         return coords
+        ## UNIT SPHERE NORMALISATION:
+        # centroid = torch.mean(coords, axis=0)
+        # coords_normalized = coords - centroid
+        # if self.scale_factor is not None:
+        #     max_distance = self.scale_factor
+        # else:
+        #     max_distance = torch.max(abs(coords_normalized)) / self.norm_range
+        # coords_normalized /= max_distance        
+        # return coords_normalized

dataset/base_datasets.py
@@ -73,7 +73,8 @@ class TrainingDataset(Dataset):
             data = self.transform(data)
         if self.load_octree:
             # Ensure no values outside of [-1, 1] exist (see ocnn documentation)
-            data = torch.clamp(data, -1, 1)
+            mask = torch.all(abs(data) <= 1.0, dim=1)
+            data = data[mask]
         return data, ndx
 
     def get_positives(self, ndx):

eval/pnv_evaluate.py
@@ -113,7 +113,7 @@ def get_latent_vectors(model, data_set, device, params: TrainingParams):
 
     if params.dataset_name in ['Oxford','Campus3D']:
         pc_loader = PNVPointCloudLoader()
-    elif 'AboveUnder' in params.dataset_name:
+    elif 'AboveUnder' in params.dataset_name or 'WildPlaces' in params.dataset_name:
         pc_loader = AboveUnderPointCloudLoader()
 
     model.eval()
@@ -125,11 +125,12 @@ def get_latent_vectors(model, data_set, device, params: TrainingParams):
         pc_file_path = os.path.join(params.dataset_folder, data_set[elem_ndx]["query"])
         data = pc_loader(pc_file_path)
         data = torch.tensor(data)
-        if params.normalize_points:
-            data = Normalize(scale=0.95)(data)
+        if params.normalize_points or params.scale_factor is not None:
+            data = Normalize(scale_factor=params.scale_factor)(data)
         if params.load_octree:  # Convert to Octree format
             # Ensure no values outside of [-1, 1] exist (see ocnn documentation)
-            data = torch.clamp(data, -1, 1)
+            mask = torch.all(abs(data) <= 1.0, dim=1)
+            data = data[mask]
             # Convert to ocnn Points object, then create Octree
             points = Points(data)
             data = Octree(params.octree_depth, full_depth=2)

eval/visualise_embeddings.py
@@ -152,7 +152,7 @@ def collate_batch(data, device, params: TrainingParams):
 def get_latent_vectors(model, data_list: list[str], device, params: TrainingParams) -> np.ndarray:
     if params.dataset_name in ['Oxford','Campus3D']:
         pc_loader = PNVPointCloudLoader()
-    elif 'AboveUnder' in params.dataset_name:
+    elif 'AboveUnder' in params.dataset_name or 'WildPlaces' in params.dataset_name:
         pc_loader = AboveUnderPointCloudLoader()
 
     bs = params.val_batch_size
@@ -163,11 +163,12 @@ def get_latent_vectors(model, data_list: list[str], device, params: TrainingPara
         pc_abs_path = os.path.join(params.dataset_folder, pc_rel_path)
         data = pc_loader(pc_abs_path)
         data = torch.tensor(data)
-        if params.normalize_points:
-            data = Normalize(scale=0.95)(data)
+        if params.normalize_points or params.scale_factor is not None:
+            data = Normalize(scale_factor=params.scale_factor)(data)
         if params.load_octree:  # Convert to Octree format
             # Ensure no values outside of [-1, 1] exist (see ocnn documentation)
-            data = torch.clamp(data, -1, 1)
+            mask = torch.all(abs(data) <= 1.0, dim=1)
+            data = data[mask]
             # Convert to ocnn Points object, then create Octree
             points = Points(data)
             data = Octree(params.octree_depth, full_depth=2)

misc/octree_window_visualisation.py
@@ -38,8 +38,8 @@ def main():
         
         points_tensor = torch.tensor(points_original, dtype=torch.float32)
         # Ensure no values outside of [-1, 1] exist (see ocnn documentation)
-        if args.normalize:
-            normalize_transform = Normalize()
+        if args.normalize or args.scale_factor is not None:
+            normalize_transform = Normalize(scale_factor=args.scale_factor)
             points_tensor = normalize_transform(points_tensor)
         points_tensor = torch.clamp(points_tensor, -1, 1)
         # Convert to ocnn Points object, then create Octree
@@ -105,6 +105,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--clouds_path', type = str, required=True, help="path to processed submaps")
     parser.add_argument('--normalize', action='store_true', help="normalize submaps if they are not already in [-1, 1] range")
+    parser.add_argument('--scale_factor', type = float, default=None, help="fixed scale factor to normalize by")
     parser.add_argument('--max_depth', type = int, required=True, help="max depth of octree to visualise")
     parser.add_argument('--min_depth', type = int, default=2, help="min depth of octree to visualise")
     parser.add_argument('--patch_size', type = int, default=32, help="size of octree windows (# points per window)")